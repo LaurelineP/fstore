41ff09d153a0e3074c020c9015ca8bec9942962c
Laureline <laurelinedev@gmail.com>
2025-08-28 08:21:55 +0200

    [ FILE-STORAGE / encoding ] - persists in db and updates thumbnail url - removed the global videoThumbnail Map - removed get video thumbnail endpoint - updated handleUploadThumbnail thumbnailURL from array buffer to base64 (stored in db)

--- src/api/thumbnails.ts
+++ src/api/thumbnails.ts
@@ -1,40 +1,11 @@
 import { getBearerToken, validateJWT } from "../auth";
 import { respondWithJSON } from "./json";
 import { getVideo, updateVideo } from "../db/videos";
+import { BadRequestError, UserForbiddenError } from "./errors";
+
 import type { ApiConfig } from "../config";
 import type { BunRequest } from "bun";
-import { BadRequestError, NotFoundError, UserForbiddenError } from "./errors";
 
-type Thumbnail = {
-  data: ArrayBuffer;
-  mediaType: string;
-};
-
-const videoThumbnails: Map<string, Thumbnail> = new Map();
-
-export async function handlerGetThumbnail(cfg: ApiConfig, req: BunRequest) {
-  const { videoId } = req.params as { videoId?: string };
-  if (!videoId) {
-    throw new BadRequestError("Invalid video ID");
-  }
-
-  const video = getVideo(cfg.db, videoId);
-  if (!video) {
-    throw new NotFoundError("Couldn't find video");
-  }
-
-  const thumbnail = videoThumbnails.get(videoId);
-  if (!thumbnail) {
-    throw new NotFoundError("Thumbnail not found");
-  }
-
-  return new Response(thumbnail.data, {
-    headers: {
-      "Content-Type": thumbnail.mediaType,
-      "Cache-Control": "no-store",
-    },
-  });
-}
 
 export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
   const { videoId } = req.params as { videoId?: string };
@@ -45,7 +16,7 @@ export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
   const token = getBearerToken(req.headers);
   const userID = validateJWT(token, cfg.jwtSecret);
 
-  console.log("uploading thumbnail for video", videoId, "by user", userID);
+  console.info("uploading thumbnail for video", videoId, "by user", userID);
 
   const reqFormData = await req.formData()
   const file = reqFormData.get('thumbnail')
@@ -63,7 +34,8 @@ export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
 
   // Prepare values to update
   const mediaType = file.type
-  const mediaBuffer = await file.arrayBuffer()
+  const mediaBuffer = Buffer.from(await file.arrayBuffer()).toString("base64")
+  const dataBufferUrl = `data:${mediaType};base64,${mediaBuffer}`
 
 
   // Retrieve video
@@ -72,14 +44,8 @@ export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
     throw new UserForbiddenError("Invalid video user")
   }
 
-  // Updates video in cache
-  videoThumbnails.set(videoData.id, {
-    data: mediaBuffer,
-    mediaType: mediaType
-  })
-
   // Thumbnail URL updated
-  const thumbnailURL = `http://localhost:${cfg.port}/api/thumbnails/${videoData.id}`
+  const thumbnailURL = dataBufferUrl
   const videoUpdates = {
     ...videoData,
     thumbnailURL
